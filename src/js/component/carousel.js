cb5253cbefa61db419ab6dbbdffbfdaaadd46a91
Bienvenido Quintna <BienvenidoQuintana34@gmail.com>
2020-05-23 16:11:33 +0000

    update

--- src/js/component/carousel.js
+++ src/js/component/carousel.js
@@ -3,7 +3,7 @@ import React from "react";
 <Carousel>
 	<Carousel.Item>
 		<img
-			className="d-block w-100",
+			className="d-block w-100"
 			src="https://thumbs.gfycat.com/CompetentMeekHydra-max-1mb.gif"
 			alt="First slide"
 		/>
